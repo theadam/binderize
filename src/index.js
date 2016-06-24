2f853bc2f527383ec84669efee179e437c94f84d
Adam Nalisnick <theadam4257@gmail.com>
2016-06-24 11:37:00 -0400

    Add different bind and apply functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-export function bindlast(fn) {
-  return function(...args) {
-    return fn(...args, this);
+export function apply(fn) {
+  return fn(this);
+}
+
+export function applyLast(fn, ...args) {
+  return fn(...args, this);
+}
+
+export function applyFirst(fn, ...args) {
+  return fn(this, ...args);
+}
+
+export function applyN(n, fn, ...args) {
+  return fn(...args.slice(0, n), this, ...args.slice(n));
+}
+
+function papplyN(n) {
+  return function(fn, ...args) {
+    return applyN.apply(this, [n, fn].concat(args));
   }
 }
 
-export function bindfirst(fn) {
+const binder = applier => fn => {
   return function(...args) {
-    return fn(...args, this);
+    return applier.apply(this, args);
   }
 }
+
+export const bindLast = binder(applyLast);
+export const bindFirst = binder(applyFirst);
+export const bindN = (n, fn) => binder(papplyN(n))(fn);
+export const bind = binder(apply);
+
